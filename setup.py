2e8163b90950a799ed40dcbf551258aa9952b724
Felipe Reyes <felipe.reyes@canonical.com>
2022-01-11 16:07:47 -0300

    Use expose-model-name branch

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ install_require = [
     'python-ceilometerclient',
     'python-cinderclient<6.0.0',
     'python-swiftclient<3.9.0',
-    'zaza@git+https://github.com/openstack-charmers/zaza.git#egg=zaza',
+    'zaza@git+https://github.com/freyes/zaza.git@expose-model-name#egg=zaza',
 ]
 
 tests_require = [
